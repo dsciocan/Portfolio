567995dc016c706e72bbeab334afa279380950cb
Stef Daniela Ciocan <daniela.ciocan137@gmail.com>
2024-11-22 18:23:39 +0000

    style: small hero changes

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -5,12 +5,12 @@ export default function Hero() {
 
 
     return (
-        <div className="pt-32 px-16 sm:px-24 mt-5" id="hero">
+        <div className="pt-32 px-12 sm:px-24 mt-5" id="hero">
             <div id="hero-highlight">
             <h1 id="highlight" className=" text-8xl sm:text-9lx lg:text-10xl font-light font-sunbru tracking-widest text-dark-red">Stef Ciocan</h1>
             </div>
             <h3 className="text-start text-2xl pt-3 font-light mt-4">Software Developer</h3>
-            <ul className="pt-5 text-4xl leading-10 space-y-4 font-semibold" id="hero-links">
+            <ul className="pt-5 text-4xl leading-10 space-y-3 font-semibold" id="hero-links">
                 <li><HashLink to={"/#about"}>ABOUT ME</HashLink></li>
                 <li><HashLink to={"/#projects"}> PROJECTS</HashLink></li>
                 <li><HashLink to={"/#contact"}>GET IN TOUCH</HashLink></li>
