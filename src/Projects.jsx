07f3df1272f07c258b578b101768a7ad93b009db
Stef Daniela Ciocan <daniela.ciocan137@gmail.com>
2025-02-24 22:50:31 +0000

    feat: add logic to filter projects in list by language used

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -4,20 +4,40 @@ import Project from "./Project";
 
 export default function Projects() {
 
-    const projects = projectList.map(pj => <Project title={pj.title} img={pj.img} link={pj.link}/>
-        
-    )
+    const [projects, setProjects] = React.useState(makeComponent(projectList))
+
+    function makeComponent(list) {
+        return list.map(pj => <Project title={pj.title} img={pj.img} link={pj.link} link2={pj.link2} title2={pj.title2} 
+            description={pj.description} technologies={pj.technologies.map(tech => tech == pj.technologies[0] ? tech : ", " + tech)}/>
+            )
+    }
+
+    function selectionFilter(selection) {
+        const filteredList = [];
+        if(selection != "Filter By Language") {
+        for(let project of projectList) {
+            if(project.technologies.includes(selection)) {
+                filteredList.push(project);
+            }
+            setProjects(makeComponent(filteredList))
+        }
+    }  else {
+        setProjects(makeComponent(projectList))
+    }
+    }
 
     return(
-            <div className=" bg-bg-light pb-16 xl:grid-cols-3 justify-evenly md:grid-cols-2 grid flex-column flex-1">
-            {projects[0]}
-            {projects[1]}
-            {projects[2]}
-            {/* </div>
-            <div className="sm:flex justify-evenly"> */}
-            {projects[3]}
-            {projects[4]}
-            {projects[5]}
+            <>
+            <div className="bg-bg-light px-16 pt-8">
+            <select onChange={e => selectionFilter(e.target.value)} name="filter" id="filter" className="py-3 px-5 border-black border-2 ">
+            <option className="italic" >Filter By Language</option>
+            <option>Java</option>
+            <option>Javascript</option>
+            </select>
             </div>
+            <div className=" bg-bg-light pb-16 xl:grid-cols-3 justify-evenly md:grid-cols-2 grid flex-column flex-1">
+            {projects}
+            </div>
+            </>
     )
 }
